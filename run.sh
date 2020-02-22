6caf285bcffaf16c4e4945d1913ea574b814b974
xxshambm <ext.michael.shamberger@tieto.com>
2020-02-22 21:37:10 +0200

    fix

--- run.sh
+++ run.sh
@@ -2,11 +2,8 @@ set -x
 BASEDIR=$1
 cd $BASEDIR
 
-mkdir -p $BASEDIR/output/csv
-mkdir -p $BASEDIR/output/fhir
-
 # run for base case with all modules
-./run_synthea -p 100 "Uusimma"
+./run_synthea -p 100 Uusimaa
 cd ./output/csv
 zip ../all_csv.zip *.csv
 cd ../..
@@ -14,14 +11,11 @@ cd ./output/fhir
 zip ../all_fhir.zip *.json
 cd ../..
 
-mkdir -p $BASEDIR/output/csv
-mkdir -p $BASEDIR/output/fhir
 for filename in $BASEDIR/build/resources/main/modules/*.json; do
     module=$(basename "$filename" .json)
-    pwd
     rm ./output/csv/*
     rm ./output/fhir/*
-    ./run_synthea -p 100 -m $module "Uusimma"
+    ./run_synthea -p 100 -m $module Uusimaa
     cd ./output/csv
     zip ../${module}_csv.zip *.csv
     cd ../..
